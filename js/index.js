fe90d8f4f4a5fc694c848ec4ffdaeacbf00cfd2c
Huy Trịnh <huytrinhm@gmail.com>
2023-01-05 18:13:01 +0700

    Add projects

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
-const aboutSection = document.getElementById("about");
+let isShowMore = false;
+const showMore = document.querySelector(".more-button");
+const otherProjects = document.querySelectorAll(".projects-grid > li");
 
+showMore.addEventListener("click", () => {
+  isShowMore = !isShowMore;
+  showMore.innerHTML = isShowMore ? "Show Less" : "Show More";
 
+  otherProjects.forEach((project, i) => {
+    if (isShowMore) project.classList.remove("project-hidden");
+    else if (i >= 6) project.classList.add("project-hidden");
+    else project.classList.remove("project-hidden");
+  });
+});
